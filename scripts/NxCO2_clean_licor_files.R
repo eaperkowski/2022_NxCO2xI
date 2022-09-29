2c79ad740410c55a0a613cb92313cfcc82d8a1b0
eaperkowski <evan.a.perkowski@ttu.edu>
2022-09-29 15:53:03 -0500

    comment write.csv lines to avoid unnecessary csv file writing

--- scripts/NxCO2_clean_licor_files.R
+++ scripts/NxCO2_clean_licor_files.R
@@ -130,53 +130,53 @@ aco2_co2Resp_w7d2_ozz <- licorData(location = "../licor_raw/week7/aco2_co2Resp_W
 # Albert
 eco2_rd_w6d1_alb <- licorData(location = "../licor_raw/week6/eco2_rd_W6D1_alb") %>%
   mutate(week = 6)
-write.csv(eco2_rd_w6d1_alb, "../licor_cleaned/rd/eco2_rd_w6d1_alb.csv", row.names = FALSE)
+# write.csv(eco2_rd_w6d1_alb, "../licor_cleaned/rd/eco2_rd_w6d1_alb.csv", row.names = FALSE)
 
 eco2_rd_w6d2_alb <- licorData(location = "../licor_raw/week6/eco2_rd_W6D2_alb") %>%
   mutate(week = 6)
-write.csv(eco2_rd_w6d2_alb, "../licor_cleaned/rd/eco2_rd_w6d2_alb.csv", row.names = FALSE)
+# write.csv(eco2_rd_w6d2_alb, "../licor_cleaned/rd/eco2_rd_w6d2_alb.csv", row.names = FALSE)
 
 aco2_rd_w6d1_alb <- licorData(location = "../licor_raw/week6/aco2_rd_W6D1_alb") %>%
   mutate(week = 6)
-write.csv(aco2_rd_w6d1_alb, "../licor_cleaned/rd/aco2_rd_w6d1_alb.csv", row.names = FALSE)
+# write.csv(aco2_rd_w6d1_alb, "../licor_cleaned/rd/aco2_rd_w6d1_alb.csv", row.names = FALSE)
 
 aco2_rd_w6d2_alb <- licorData(location = "../licor_raw/week6/aco2_rd_W6D2_alb") %>%
   mutate(week = 6)
-write.csv(aco2_rd_w6d2_alb, "../licor_cleaned/rd/aco2_rd_w6d2_alb.csv", row.names = FALSE)
+# write.csv(aco2_rd_w6d2_alb, "../licor_cleaned/rd/aco2_rd_w6d2_alb.csv", row.names = FALSE)
 
 # Stan
 eco2_rd_w6d1_stan <- licorData(location = "../licor_raw/week6/eco2_rd_W6D1_stan") %>%
   mutate(week = 6)
-write.csv(eco2_rd_w6d1_stan, "../licor_cleaned/rd/eco2_rd_w6d1_stan.csv", row.names = FALSE)
+# write.csv(eco2_rd_w6d1_stan, "../licor_cleaned/rd/eco2_rd_w6d1_stan.csv", row.names = FALSE)
 
 eco2_rd_w6d2_stan <- licorData(location = "../licor_raw/week6/eco2_rd_W6D2_stan") %>%
   mutate(week = 6)
-write.csv(eco2_rd_w6d2_stan, "../licor_cleaned/rd/eco2_rd_w6d2_stan.csv", row.names = FALSE)
+# write.csv(eco2_rd_w6d2_stan, "../licor_cleaned/rd/eco2_rd_w6d2_stan.csv", row.names = FALSE)
 
 aco2_rd_w6d1_stan <- licorData(location = "../licor_raw/week6/aco2_rd_W6D1_stan") %>%
   mutate(week = 6)
-write.csv(aco2_rd_w6d1_stan, "../licor_cleaned/rd/aco2_rd_w6d1_stan.csv", row.names = FALSE)
+# write.csv(aco2_rd_w6d1_stan, "../licor_cleaned/rd/aco2_rd_w6d1_stan.csv", row.names = FALSE)
 
 aco2_rd_w6d2_stan <- licorData(location = "../licor_raw/week6/aco2_rd_W6D2_stan") %>%
   mutate(week = 6)
-write.csv(aco2_rd_w6d2_stan, "../licor_cleaned/rd/aco2_rd_w6d2_stan.csv", row.names = FALSE)
+# write.csv(aco2_rd_w6d2_stan, "../licor_cleaned/rd/aco2_rd_w6d2_stan.csv", row.names = FALSE)
 
 # Ozzie
 eco2_rd_w6d1_ozz <- licorData(location = "../licor_raw/week6/eco2_rd_W6D1_ozz") %>%
   mutate(week = 6)
-write.csv(eco2_rd_w6d1_ozz, "../licor_cleaned/rd/eco2_rd_w6d1_ozz.csv", row.names = FALSE)
+# write.csv(eco2_rd_w6d1_ozz, "../licor_cleaned/rd/eco2_rd_w6d1_ozz.csv", row.names = FALSE)
 
 eco2_rd_w6d2_ozz <- licorData(location = "../licor_raw/week6/eco2_rd_W6D2_ozz") %>%
   mutate(week = 6)
-write.csv(eco2_rd_w6d2_ozz, "../licor_cleaned/rd/eco2_rd_w6d2_ozz.csv", row.names = FALSE)
+# write.csv(eco2_rd_w6d2_ozz, "../licor_cleaned/rd/eco2_rd_w6d2_ozz.csv", row.names = FALSE)
 
 aco2_rd_w6d1_ozz <- licorData(location = "../licor_raw/week6/aco2_rd_W6D1_ozz") %>%
   mutate(week = 6)
-write.csv(aco2_rd_w6d1_ozz, "../licor_cleaned/rd/aco2_rd_w6d1_ozz.csv", row.names = FALSE)
+# write.csv(aco2_rd_w6d1_ozz, "../licor_cleaned/rd/aco2_rd_w6d1_ozz.csv", row.names = FALSE)
 
 aco2_rd_w6d2_ozz <- licorData(location = "../licor_raw/week6/aco2_rd_W6D2_ozzie") %>%
   mutate(week = 6)
-write.csv(aco2_rd_w6d2_ozz, "../licor_cleaned/rd/aco2_rd_w6d2_ozz.csv", row.names = FALSE)
+# write.csv(aco2_rd_w6d2_ozz, "../licor_cleaned/rd/aco2_rd_w6d2_ozz.csv", row.names = FALSE)
 
 ###############################################################################
 ## Load dark respiration data files for week 7. Adding column to indicate 
@@ -186,53 +186,53 @@ write.csv(aco2_rd_w6d2_ozz, "../licor_cleaned/rd/aco2_rd_w6d2_ozz.csv", row.name
 # Albert
 eco2_rd_w7d1_alb <- licorData(location = "../licor_raw/week7/eco2_rd_W7D1_alb") %>%
   mutate(week = 7)
-write.csv(eco2_rd_w7d1_alb, "../licor_cleaned/rd/eco2_rd_w7d1_alb.csv", row.names = FALSE)
+# write.csv(eco2_rd_w7d1_alb, "../licor_cleaned/rd/eco2_rd_w7d1_alb.csv", row.names = FALSE)
 
 eco2_rd_w7d2_alb <- licorData(location = "../licor_raw/week7/eco2_rd_W7D2_alb") %>%
   mutate(week = 7)
-write.csv(eco2_rd_w7d2_alb, "../licor_cleaned/rd/eco2_rd_w7d2_alb.csv", row.names = FALSE)
+# write.csv(eco2_rd_w7d2_alb, "../licor_cleaned/rd/eco2_rd_w7d2_alb.csv", row.names = FALSE)
 
 aco2_rd_w7d1_alb <- licorData(location = "../licor_raw/week7/aco2_rd_W7D1_alb") %>%
   mutate(week = 7)
-write.csv(aco2_rd_w7d1_alb, "../licor_cleaned/rd/aco2_rd_w7d1_alb.csv", row.names = FALSE)
+# write.csv(aco2_rd_w7d1_alb, "../licor_cleaned/rd/aco2_rd_w7d1_alb.csv", row.names = FALSE)
 
 aco2_rd_w7d2_alb <- licorData(location = "../licor_raw/week7/aco2_rd_W7D2_alb") %>%
   mutate(week = 7)
-write.csv(aco2_rd_w7d2_alb, "../licor_cleaned/rd/aco2_rd_w7d2_alb.csv", row.names = FALSE)
+# write.csv(aco2_rd_w7d2_alb, "../licor_cleaned/rd/aco2_rd_w7d2_alb.csv", row.names = FALSE)
 
 # Stan
 eco2_rd_w7d1_stan <- licorData(location = "../licor_raw/week7/eco2_rd_W7D1_stan") %>%
   mutate(week = 7)
-write.csv(eco2_rd_w7d1_stan, "../licor_cleaned/rd/eco2_rd_w7d1_stan.csv", row.names = FALSE)
+# write.csv(eco2_rd_w7d1_stan, "../licor_cleaned/rd/eco2_rd_w7d1_stan.csv", row.names = FALSE)
 
 eco2_rd_w7d2_stan <- licorData(location = "../licor_raw/week7/eco2_rd_W7D2_stan") %>%
   mutate(week = 7)
-write.csv(eco2_rd_w7d2_stan, "../licor_cleaned/rd/eco2_rd_w7d2_stan.csv", row.names = FALSE)
+# write.csv(eco2_rd_w7d2_stan, "../licor_cleaned/rd/eco2_rd_w7d2_stan.csv", row.names = FALSE)
 
 aco2_rd_w7d1_stan <- licorData(location = "../licor_raw/week7/aco2_rd_W7D1_stan") %>%
   mutate(week = 7)
-write.csv(aco2_rd_w7d1_stan, "../licor_cleaned/rd/aco2_rd_w7d1_stan.csv", row.names = FALSE)
+# write.csv(aco2_rd_w7d1_stan, "../licor_cleaned/rd/aco2_rd_w7d1_stan.csv", row.names = FALSE)
 
 aco2_rd_w7d2_stan <- licorData(location = "../licor_raw/week7/aco2_rd_W7D2_stan") %>%
   mutate(week = 7)
-write.csv(aco2_rd_w7d2_stan, "../licor_cleaned/rd/aco2_rd_w7d2_stan.csv", row.names = FALSE)
+# write.csv(aco2_rd_w7d2_stan, "../licor_cleaned/rd/aco2_rd_w7d2_stan.csv", row.names = FALSE)
 
 # Ozzie
 eco2_rd_w7d1_ozz <- licorData(location = "../licor_raw/week7/eco2_rd_W7D1_ozz") %>%
   mutate(week = 7)
-write.csv(eco2_rd_w7d1_ozz, "../licor_cleaned/rd/eco2_rd_w7d1_ozz.csv", row.names = FALSE)
+# write.csv(eco2_rd_w7d1_ozz, "../licor_cleaned/rd/eco2_rd_w7d1_ozz.csv", row.names = FALSE)
 
 eco2_rd_w7d2_ozz <- licorData(location = "../licor_raw/week7/eco2_rd_W7D2_ozz") %>%
   mutate(week = 7)
-write.csv(eco2_rd_w7d2_ozz, "../licor_cleaned/rd/eco2_rd_w7d2_ozz.csv", row.names = FALSE)
+# write.csv(eco2_rd_w7d2_ozz, "../licor_cleaned/rd/eco2_rd_w7d2_ozz.csv", row.names = FALSE)
 
 aco2_rd_w7d1_ozz <- licorData(location = "../licor_raw/week7/aco2_rd_W7D1_ozz") %>%
   mutate(week = 7)
-write.csv(aco2_rd_w7d1_ozz, "../licor_cleaned/rd/aco2_rd_w7d1_ozz.csv", row.names = FALSE)
+# write.csv(aco2_rd_w7d1_ozz, "../licor_cleaned/rd/aco2_rd_w7d1_ozz.csv", row.names = FALSE)
 
 aco2_rd_w7d2_ozz <- licorData(location = "../licor_raw/week7/aco2_rd_W7D2_ozz") %>%
   mutate(week = 7)
-write.csv(aco2_rd_w7d2_ozz, "../licor_cleaned/rd/aco2_rd_w7d2_ozz.csv", row.names = FALSE)
+# write.csv(aco2_rd_w7d2_ozz, "../licor_cleaned/rd/aco2_rd_w7d2_ozz.csv", row.names = FALSE)
 
 ###############################################################################
 ## Merge files into central file. Useful for 'fitacis' when fitting multiple
